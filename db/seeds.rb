efa2269c0b4683e634e5bdc29b3284930de035a0
DevNeoLee <devneolee@gmail.com>
2020-11-30 10:53:17 -0800

    Revert "switch db sql to postgresql"
    This reverts commit 8564e00ebbf8abb83a8dda61038ddf85332710c7.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+25.times do
+    Room.create(user_id: 1, home_type: "house", room_type: "good_room", accomodate: "yes", bed_room: 1, bath_room: 2, listing_name: "good", summary: "yeah I told you it's a good room", address: "canada")
+end
+
