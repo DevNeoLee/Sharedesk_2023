d693dee96c73a1281c10abf03b8680c95096f879
DevNeoLee <devneolee@gmail.com>
2020-11-30 10:36:46 -0800

    conflic resolution

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,13 @@ module Sharedesk
 
     config.load_defaults 6.0
 
+<<<<<<< HEAD
     config.generators.system_tests = nil
+=======
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+>>>>>>> parent of ebb36af... debug when compiling// fix message_controller miss-spell, delete js/site.js, config application.rb production.rb webpacker.yml
   end
 end
