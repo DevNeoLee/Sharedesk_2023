2d4631460f124e334ab696679dda254888c8db15
DevNeoLee <devneolee@gmail.com>
2020-12-05 20:12:36 -0800

    update pagination quanitity

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
     def show 
         @user = User.find(params[:id])
         @rooms = @user.rooms
+        @pagy, @rooms_result = pagy(@rooms, items: 10)
     end 
 end
