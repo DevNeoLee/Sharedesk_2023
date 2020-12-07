c5543ffd5f63a3851cc16601f9477c9067b0ff90
DevNeoLee <devneolee@gmail.com>
2020-12-07 05:35:45 -0800

    change review logic for home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
     @pagy, @rooms = pagy(Room.all, items: 3)
     @search = @rooms.ransack(params[:q])
     @reviews = Review.all
-    @best_reviews = @reviews[4..7]
+    @best_reviews = @reviews[37..39]
     @best_rooms = User.find(2).rooms[0...3]
   end
 
